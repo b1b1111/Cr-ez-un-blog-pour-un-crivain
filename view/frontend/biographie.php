a6dae4661978f1c5341d7c39472486a123c743f5
Benjamin Lefebvre <49646590+b1b1111@users.noreply.github.com>
2019-05-29 14:09:53 +0200

    Add files via upload

---
+++ view/frontend/biographie.php
@@ -0,0 +1,31 @@
+<?php $title = 'Jean Forteroche'; ?>
+<?php require('header.php'); ?>
+
+<?php ob_start(); ?>
+
+<div id="bio">
+    <h2>Biographie</h2>
+
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique convallis eleifend. Aliquam vel varius metus. Suspendisse tincidunt, lorem in blandit elementum, quam nisi vestibulum libero, at fringilla arcu nunc vitae ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pulvinar magna eget ex elementum, vitae sollicitudin arcu rutrum. Nullam ut aliquet risus. Sed eu lectus egestas, lacinia arcu vel, ornare dolor. Fusce lobortis egestas efficitur. Donec consequat felis a bibendum dapibus. Vivamus consequat metus nec dolor placerat, quis pulvinar urna pulvinar. Proin pretium bibendum lorem a molestie. Donec vitae nisl nulla. Nullam tempor quam eu libero tincidunt, nec iaculis risus rhoncus. Sed gravida eros ac odio luctus, sed ornare ante mollis.
+    </p>
+    <p>
+    Proin quis dignissim erat, quis iaculis est. Suspendisse convallis consequat felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend pretium aliquam. Fusce viverra nisi a ipsum elementum, non volutpat elit volutpat. Vivamus sed sapien tincidunt, consectetur justo sed, efficitur lectus. Nullam at aliquet enim, eget ullamcorper ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce posuere augue ut purus convallis dictum. Vivamus consequat mauris cursus nibh vehicula, non placerat arcu vestibulum. Morbi ultrices eros sed mollis commodo. Proin facilisis velit eleifend fringilla faucibus. Mauris hendrerit eu turpis suscipit hendrerit. Praesent vel nulla facilisis, varius augue non, luctus nisl.
+    </p>
+    <p>
+    In erat enim, fermentum ut orci id, convallis iaculis nunc. Sed fringilla odio ac mi feugiat commodo. Donec dapibus consequat blandit. Fusce elementum felis neque, vitae interdum risus blandit posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque scelerisque odio eget ante bibendum iaculis. Vestibulum a interdum justo. Cras ut lacus sodales, tincidunt libero sed, gravida ex. Donec egestas magna quam, ut dapibus turpis venenatis ut. Fusce sed diam elementum, rutrum sem eget, semper eros. Mauris id mauris quis leo commodo euismod.
+    </p>
+    <p>
+    Etiam cursus erat in tortor gravida facilisis. Donec consectetur pharetra purus, nec consectetur est condimentum a. Donec metus ante, feugiat quis ex quis, lobortis fringilla tortor. Mauris pharetra erat eget augue egestas placerat. Nullam tristique velit ac finibus aliquet. Morbi auctor purus id ligula aliquet eleifend. Integer hendrerit venenatis sollicitudin. Pellentesque at nibh eu erat tincidunt iaculis. Nam pretium, tellus ac lacinia blandit, mi nibh eleifend magna, nec ornare tortor dolor id nunc. Aenean lobortis blandit risus, quis posuere augue egestas sit amet. Vivamus ultrices turpis in faucibus feugiat.
+    </p>
+    <p>
+    Sed non risus tristique urna blandit lobortis vel vel ligula. Integer vel fermentum arcu. Proin quam mi, commodo eu mattis hendrerit, porta in neque. Nullam ut semper sapien, vitae blandit lacus. Vestibulum ut convallis purus, varius ornare dui. Aliquam iaculis felis eget euismod mattis. Etiam eu nulla luctus, porta ipsum id, varius diam.
+    </p>
+    <p>
+    Donec dictum nulla elit, ut suscipit ligula congue ut. Suspendisse quam dolor, hendrerit eu eleifend et, consequat venenatis nulla. Morbi eu molestie urna. Cras sollicitudin tempus turpis ut faucibus. Proin vehicula orci ut leo commodo congue. Sed id velit congue, consequat justo non, malesuada nisi. Pellentesque quis tortor sit amet sapien euismod tincidunt.
+    </p>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
